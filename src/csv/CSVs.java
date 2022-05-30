c97ef562885c04b7ae5cbc80ff4dacda68f5b8e1
SDWoo <damjang67@naver.com>
2022-05-30 21:22:57 +0900

    First Commit

---
+++ src/csv/CSVs.java
@@ -0,0 +1,36 @@
+package csv;
+
+import java.io.*;
+import java.io.FileNotFoundException;
+
+public class CSVs {
+    /**
+     * @param isFirstLineHeader csv 파일의 첫 라인을 헤더(타이틀)로 처리할까요?
+     */
+    public static Table createTable(File csv, boolean isFirstLineHeader) throws FileNotFoundException {
+        Table table;
+        BufferedReader reader = new BufferedReader(new FileReader(csv));
+
+        if (isFirstLineHeader == true ) {
+            
+        }
+        else {
+
+        }
+        return null;
+    }
+
+    /**
+     * @return 새로운 Table 객체를 반환한다. 즉, 첫 번째 매개변수 Table은 변경되지 않는다.
+     */
+    public static Table sort(Table table, int byIndexOfColumn, boolean isAscending, boolean isNullFirst) {
+        return null;
+    }
+
+    /**
+     * @return 새로운 Table 객체를 반환한다. 즉, 첫 번째 매개변수 Table은 변경되지 않는다.
+     */
+    public static Table shuffle(Table table) {
+        return null;
+    }
+}
